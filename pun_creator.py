f62377c82ebd723b378ffa361564cf5a1a63d177
Kaimyn Chapman <kchap21@hotmail.com>
2017-06-09 00:50:05 +0100

    Updated to allow searching by regex

--- pun_creator.py
+++ pun_creator.py
@@ -21,16 +21,41 @@ with open('cmudict-0.7b','r') as pron_file:
         pronunciation = pronunciation[1:] # We copy with leading space
         pron_dict[word] += [pronunciation]
 
+def generate_pattern(word):
+    '''Gives the pronunciation for a word'''
+    valid_prons = pron_dict[word.upper()]
+    if len(valid_prons) == 0:
+        raise ValueError(
+            'The word {} does not exist in the dictionary'.format(word)
+            )
+    else:
+        return valid_prons
     
-def pattern_search(pattern):
-    '''Returns all words containing "pattern" in their pronunciation
+##def pattern_search_base(pattern):
+##    '''Returns all words containing "pattern" in their pronunciation
+##Give patten in form e.g. "EY B"'''
+##    words_with_pattern = []
+##    
+##    for word in pron_dict:
+##        has_pattern = 0
+##        for pron in pron_dict[word]:
+##            if pattern in pron:
+##                has_pattern = 1
+##                break
+##        if has_pattern:
+##            words_with_pattern += [word]
+##            continue
+##    return sorted(words_with_pattern)
+
+def pattern_search(regex):
+    '''Returns all words whose pronunciation matches the regex
 Give patten in form e.g. "EY B"'''
     words_with_pattern = []
     
     for word in pron_dict:
         has_pattern = 0
         for pron in pron_dict[word]:
-            if pattern in pron:
+            if re.search(regex, pron) is not None:
                 has_pattern = 1
                 break
         if has_pattern:
@@ -38,5 +63,12 @@ Give patten in form e.g. "EY B"'''
             continue
     return sorted(words_with_pattern)
 
+def word_search(word):
+    '''Searches for the words that, when pronounced, contain input word'''
+    pattern = generate_pattern(word)[0]
+    return pattern_search(pattern)
+
 # Search for Eero puns
-print(pattern_search('EH1 R OW0'))
+# print(pattern_search_base('EH1 R OW0'))
+# print(word_search('EROH'))
+print(pattern_search('EH[0-9] R[0-9]{0,1} OW[0-9]'))
